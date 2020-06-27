28640a69009b07a0d8598454b4cc33ff0f2e4709
Austin Rich <austin.rich@e-hps.com>
2020-06-27 18:35:01 -0400

    Last rollback

--- api/AuthorizeBox/index.js
+++ api/AuthorizeBox/index.js
@@ -1,13 +1,8 @@
 module.exports = async function (context, req) {
-    const response = getClientCredentials();
-
     context.res = {
       body: {
         text: "Hello from the API"
       }
     };
-  };
-
-const getClientCredentials = () => {
-    return false;
 };
+
