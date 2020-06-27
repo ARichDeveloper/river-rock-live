40013baa9d358b33dab0f29e6ed1b06605f9e456
Austin Rich <austin.rich@e-hps.com>
2020-06-27 17:31:30 -0400

    Text API commit

--- api/AuthorizeBox/index.js
+++ api/AuthorizeBox/index.js
@@ -1,7 +1,21 @@
+const axios = require('axios');
+const oauth = require('axios-oauth-client');
+
 module.exports = async function (context, req) {
+    const response = await getClientCredentials();
+
     context.res = {
       body: {
         text: "Hello from the API"
       }
     };
   };
+
+const getClientCredentials = oauth.client(axios.create(), {
+    url: 'https://auth.boxcast.com/oauth2/token',
+    grant_type: 'client_credentials',
+    client_id: 'ZZZ',
+    client_secret: 'ZZZ',
+    scope: 'owner'
+});
+
