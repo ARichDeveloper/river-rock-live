7b71302d7dd1ffb9da6d326395be1dddd6dc0312
Austin Rich <ARichDeveloper@gmail.com>
2020-07-29 22:03:24 -0400

    Added box endpoint to fetch channels

--- api/AuthenticateBox/index.js
+++ api/AuthenticateBox/index.js
@@ -2,17 +2,16 @@ const axios = require('axios');
 const config = require('./../config.json');
 
 module.exports = async function (context, req) {
-
-  const channels = axios.get(`${config.API_ROOT}/accounts/${config.ACCOUNT_ID}/channels`, {}).then(response => {
-    return {
+  const preroll = await axios.get(`${config.API_ROOT}/channels/${config.CHANNEL_ID}/broadcasts?s=-starts_at`, {}).then(response => {
+    const channels = {
       pagination: JSON.parse(response.headers['x-pagination'] || '{}'),
       data: response.data
     };
-  });
 
-  context.res = {
-    body: {
-      channels
-    }
-  };
+    context.res = {
+      body: {
+        channels
+      }
+    };
+  });
 };
